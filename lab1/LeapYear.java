b2756a7990a9416ec23ae39bb18eab9cbfa19e03
HLGZJY <heguojun_0419@qq.com>
2024-03-10 11:11:15 +0800

    第二次提交测试

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,7 +6,7 @@ public class LeapYear {
     public static boolean isLeapYear(int year){
         return ((year%4==0&&year%100!=0)||year%400==0);
    }
-    /** Calls isLeapYear to print correct statement.
+    /** Calls isLeapYear to print correct statement.修改点
      *  @param  year to be analyzed
      */
     private static void checkLeapYear(int year) {
